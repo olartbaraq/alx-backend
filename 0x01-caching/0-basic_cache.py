673bf0f7751328b68ce9f15af4404f84c9f9a27f
olart <akanbi.ges201@gmail.com>
2022-07-12 16:42:30 +0100

    implementing Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+
+
+class BaseCaching():
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        raise NotImplementedError("put must be implemented in\
+your cache class")
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        raise NotImplementedError("get must be implemented in\
+your cache class")
+
+
+class BasicCache(BaseCaching):
+    """a new class inherited from base class"""
+    def put(self, key, item):
+        """add an item in key-value pair to the cache"""
+        if key or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get an item by key from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
