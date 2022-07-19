90b7edbc38f988017a2695e1c9359cf772039fd6
olart <akanbi.ges201@gmail.com>
2022-07-19 17:13:56 +0100

    task 0 + pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Starts a Flash Web Application """
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def homepage():
     """returns the html page for home"""
     return render_template('0-index.html')
@@ -12,4 +12,4 @@ def homepage():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
